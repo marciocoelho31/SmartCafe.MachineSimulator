8daf89187e9f2b9957f1c0ee08644e8ee2e5c4fe
Marcio Coelho Socorro <m.coelho.socorro@avanade.com>
2021-04-18 10:07:45 -0300

    Add project files.

---
+++ SmartCafe.EventHub.Sender/CoffeeMachineDataSender.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using SmartCafe.EventHub.Sender.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCafe.EventHub.Sender
+{
+    public interface ICoffeeMachineDataSender
+    {
+        Task SendDataAsync(CoffeeMachineData data);
+        Task SendDataAsync(IEnumerable<CoffeeMachineData> data);
+    }
+
+    public class CoffeeMachineDataSender : ICoffeeMachineDataSender
+    {
+        private EventHubClient _eventHubClient;
+
+        public CoffeeMachineDataSender(string eventHubConnectionString)
+        {
+            _eventHubClient = EventHubClient.CreateFromConnectionString(eventHubConnectionString);
+        }
+
+        public async Task SendDataAsync(CoffeeMachineData data)
+        {
+            EventData eventData = CreateEventData(data);
+            await _eventHubClient.SendAsync(eventData);
+        }
+
+        public async Task SendDataAsync(IEnumerable<CoffeeMachineData> datas)
+        {
+            var eventDatas = datas.Select(coffeeMachineData => CreateEventData(coffeeMachineData));
+
+            var eventDataBatch = _eventHubClient.CreateBatch();
+            
+            foreach(var eventData in eventDatas)
+            {
+                if (!eventDataBatch.TryAdd(eventData))
+                {
+                    await _eventHubClient.SendAsync(eventDataBatch.ToEnumerable());
+                    eventDataBatch = _eventHubClient.CreateBatch();
+                    eventDataBatch.TryAdd(eventData);
+                }
+            }
+
+            if (eventDataBatch.Count > 0)
+                await _eventHubClient.SendAsync(eventDataBatch.ToEnumerable());
+        }
+
+        private static EventData CreateEventData(CoffeeMachineData data)
+        {
+            var dataAsJson = JsonConvert.SerializeObject(data);
+            var eventData = new EventData(Encoding.UTF8.GetBytes(dataAsJson));
+            return eventData;
+        }
+
+    }
+}
---
+++ SmartCafe.MachineSimulator.UI/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using SmartCafe.EventHub.Sender;
+using SmartCafe.MachineSimulator.UI.ViewModel;
+using System.Configuration;
+using System.Windows;
+
+namespace SmartCafe.MachineSimulator.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var eventHubConnectionString =
+                ConfigurationManager.AppSettings["EventHubConnectionString"];
+            DataContext = new MainViewModel(new CoffeeMachineDataSender(eventHubConnectionString));
+        }
+    }
+}
